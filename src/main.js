3558c6dcc52d12c33ea3fb3334d6562dd3a46bbc
Tati Shevchenko <tatilunarsnail@gmail.com>
2024-07-17 14:28:37 +0300

    Join 2 lines into 1 before if section

--- src/main.js
+++ src/main.js
@@ -24,44 +24,42 @@ form.addEventListener("submit", (event) => {
             })
         return;
     }
-        loader.style.display = "inline-block";
-        fetchImages(searchValue)
-        .then((images) => {
-            if (images.hits.length === 0) {
-                galleryList.innerHTML = "";
-                iziToast.error({
-                    title: '',
-                    icon: '',
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                    position: 'topCenter',
-                    backgroundColor: "#ef4040",
-                    titleColor: "#fff",
-                    messageColor: "#fff"
-                    })
-            } else {
-                galleryList.innerHTML = "";
-                galleryList.insertAdjacentHTML("beforeend", renderImages(images.hits));
-                const links = document.querySelectorAll(".gallery-link");
-                links.forEach(link => link.addEventListener("click", event => event.preventDefault()));
-                const lightbox = new SimpleLightbox('.gallery a', {
-                    captionsData: "alt",
-                    captionPosition: "bottom",
-                    captionDelay: 250
-                });
-                lightbox.refresh();
-            }
+    loader.style.display = "inline-block";
+    fetchImages(searchValue)
+    .then((images) => {
+        galleryList.innerHTML = "";
+        if (images.hits.length === 0) {
+            iziToast.error({
+                title: '',
+                icon: '',
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                position: 'topCenter',
+                backgroundColor: "#ef4040",
+                titleColor: "#fff",
+                messageColor: "#fff"
+                })
+        } else {
+            galleryList.insertAdjacentHTML("beforeend", renderImages(images.hits));
+            const links = document.querySelectorAll(".gallery-link");
+            links.forEach(link => link.addEventListener("click", event => event.preventDefault()));
+            const lightbox = new SimpleLightbox('.gallery a', {
+                captionsData: "alt",
+                captionPosition: "bottom",
+                captionDelay: 250
+            });
+            lightbox.refresh();
+        }
     })
-        .catch((error) => iziToast.error({
-            title: '',
-            icon: '',
-            message: `${error}`,
-            position: 'topCenter',
-            backgroundColor: "#ef4040",
-            titleColor: "#fff",
-            messageColor: "#fff"
-            })
-        ).finally(() => {
-            loader.style.display = "none";
+    .catch((error) => iziToast.error({
+        title: '',
+        icon: '',
+        message: `${error}`,
+        position: 'topCenter',
+        backgroundColor: "#ef4040",
+        titleColor: "#fff",
+        messageColor: "#fff"
         })
-    }
-);
+    ).finally(() => {
+        loader.style.display = "none";
+    })
+});
