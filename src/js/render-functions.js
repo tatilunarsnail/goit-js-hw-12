47ccb587b62e99d08f6684a0268a6b6e34b77f53
Tati Shevchenko <tatilunarsnail@gmail.com>
2024-07-18 02:25:07 +0300

    Ind fix

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,24 +1,32 @@
 export function renderImages(array) {
-    return array
-    .map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) =>
-         `<li class="gallery-item">
-              <a class="gallery-link" href="${largeImageURL}">
-              <img
-              class="gallery-image"
-              src="${webformatURL}"
-              data-source="${largeImageURL}"
-              alt="${tags}"
-              width="360px"
-              height="152px"
-              />
-              </a>
-              <div class="info-box">
-                  <p>Like: <span>${likes}</span></p>
-                  <p>Views: <span>${views}</span></p>
-                  <p>Comments: <span>${comments}</span></p>
-                  <p>Downloads: <span>${downloads}</span></p>
-              </div>
-        </li>`
+  return array
+      .map(({
+              webformatURL,
+              largeImageURL,
+              tags,
+              likes,
+              views,
+              comments,
+              downloads
+          }) =>
+          `<li class="gallery-item">
+            <a class="gallery-link" href="${largeImageURL}">
+            <img
+            class="gallery-image"
+            src="${webformatURL}"
+            data-source="${largeImageURL}"
+            alt="${tags}"
+            width="360px"
+            height="152px"
+            />
+            </a>
+            <div class="info-box">
+                <p>Like: <span>${likes}</span></p>
+                <p>Views: <span>${views}</span></p>
+                <p>Comments: <span>${comments}</span></p>
+                <p>Downloads: <span>${downloads}</span></p>
+            </div>
+      </li>`
       )
-    .join("");
-  }
+      .join("");
+}
